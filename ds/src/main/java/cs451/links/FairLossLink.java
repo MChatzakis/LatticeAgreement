40bb7a881c049cf43af354f76afddc7fc1cc0932
Manolis Chatzakis <chatzakis@ics.forth.gr>
2022-11-24 13:49:16 +0100

    deleting unused classes

--- ds/src/main/java/cs451/links/FairLossLink.java
+++ ds/src/main/java/cs451/links/FairLossLink.java
@@ -2,9 +2,7 @@ package cs451.links;
 
 import cs451.Constants;
 import cs451.Host;
-import cs451.network.LightUDPSender;
 import cs451.network.UDPReceiver;
-import cs451.network.UDPSender;
 import cs451.structures.Deliverer;
 import cs451.messaging.Message;
 
@@ -17,13 +15,11 @@ import java.util.ArrayList;
 public class FairLossLink extends Link{
 
     private UDPReceiver receiver;
-    //private UDPSender sender;
     private Thread receiverThread;
 
     public FairLossLink(Deliverer deliverer, int port) throws SocketException {
         this.deliverer = deliverer;
         this.receiver = new UDPReceiver(port, this);
-        //this.sender = new UDPSender();
     }
 
     private void runReceiverThread(){
@@ -40,7 +36,6 @@ public class FairLossLink extends Link{
         if(Constants.FLL_MESSAGING_VERBOSE){
             System.out.println("[FairLossLink]: Sent " + message);
         }
-        //new LightUDPSender().send(message, host.getIp(), host.getPort());
         receiver.send(message, host.getIp(), host.getPort());
     }
 
@@ -49,9 +44,6 @@ public class FairLossLink extends Link{
         if(Constants.FLL_MESSAGING_VERBOSE){
             System.out.println("[FairLossLink]: Sent " + batch);
         }
-
-        //new LightUDPSender().sendBatch(batch, host.getIp(), host.getPort());
-        //sender.sendBatch(batch, host.getIp(), host.getPort());
         receiver.sendBatch(batch, host.getIp(), host.getPort());
     }
 
